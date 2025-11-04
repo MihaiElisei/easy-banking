17088a77e2350105ed58846bee57d15fc7b752fa
Mihai <mihai.m.elisei@gmail.com>
2025-11-05 00:55:09 +0200

    Implemented services and controllers for loans

--- loans/src/main/java/com/eazybanking/loans/LoansApplication.java
+++ loans/src/main/java/com/eazybanking/loans/LoansApplication.java
@@ -1,9 +1,33 @@
 package com.eazybanking.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Easy Bank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Mihai Elisei",
+                        email = "mihai.m.elisei@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Easy Bank Loans microservice REST API Documentation"
+        )
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
