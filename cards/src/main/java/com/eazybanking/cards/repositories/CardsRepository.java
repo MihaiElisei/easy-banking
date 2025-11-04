6674515bf990c828da62a0d9be2c7be3cc2b04c2
Mihai <mihai.m.elisei@gmail.com>
2025-11-04 17:17:04 +0200

    Added Repository and DTOs for cards-ms

---
+++ cards/src/main/java/com/eazybanking/cards/repositories/CardsRepository.java
@@ -0,0 +1,12 @@
+package com.eazybanking.cards.repositories;
+
+import com.eazybanking.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
