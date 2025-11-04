78cc7260b26242cc3e53486cff86ca0666c0be42
Mihai <mihai.m.elisei@gmail.com>
2025-11-04 13:30:04 +0200

    handle exception during runtime, added validation for DTOs

--- accounts/src/main/java/com/eazybaniking/accounts/DTO/CustomerDTO.java
+++ accounts/src/main/java/com/eazybaniking/accounts/DTO/CustomerDTO.java
@@ -1,11 +1,24 @@
 package com.eazybaniking.accounts.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
+
+    @NotNull(message = "Name can not be null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
+
+    @NotNull(message = "Email address can not be null or empty")
+    @Email(message = "Email address should be a valid value")
     private String email;
+
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
+
     private AccountsDTO accountsDTO;
 }
