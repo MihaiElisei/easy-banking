ee5b85a379b56b5708e0ff918ef526d0eac2a0af
Mihai <mihai.m.elisei@gmail.com>
2025-11-04 16:26:03 +0200

    added API documentation with swagger

--- accounts/src/main/java/com/eazybaniking/accounts/DTO/AccountsDTO.java
+++ accounts/src/main/java/com/eazybaniking/accounts/DTO/AccountsDTO.java
@@ -1,19 +1,36 @@
 package com.eazybaniking.accounts.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDTO {
 
     @NotEmpty(message = "Account number can not be null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
+    @Schema(
+            description = "Account number",
+            example = "2568123571"
+    )
     private Long accountNumber;
 
     @NotEmpty(message = "Account Type can not be null or empty")
+    @Schema(
+            description = "Account Type",
+            example = "SAVINGS"
+    )
     private String accountType;
 
     @NotEmpty(message = "Branch Address can not be null or empty")
+    @Schema(
+            description = "Easy Bank branch address",
+            example = "123 New York"
+    )
     private String branchAddress;
 }
--- accounts/src/main/java/com/eazybaniking/accounts/controllers/AccountsController.java
+++ accounts/src/main/java/com/eazybaniking/accounts/controllers/AccountsController.java
@@ -1,8 +1,15 @@
 package com.eazybaniking.accounts.controllers;
 
 import com.eazybaniking.accounts.DTO.CustomerDTO;
+import com.eazybaniking.accounts.DTO.ErrorResponseDTO;
 import com.eazybaniking.accounts.DTO.ResponseDTO;
 import com.eazybaniking.accounts.services.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -12,9 +19,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import static com.eazybaniking.accounts.constants.AccountsConstants.*;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.EXPECTATION_FAILED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in Easy Bank",
+        description = "CRUD REST APIs in Easy Bank to CREATE, UPDATE, FETCH and DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -23,6 +34,13 @@ public class AccountsController {
 
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer & Account inside Easy Bank"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Account created successfully")
+    })
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         iAccountsService.createAccount(customerDTO);
@@ -31,6 +49,13 @@ public class AccountsController {
                 .body(new ResponseDTO(STATUS_201, MESSAGE_200));
     }
 
+    @Operation(
+            summary = "Fetch Account details REST API",
+            description = "REST API to fetch Customer & Account details from DB"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Fetch Customer & Account details from DB")
+    })
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                                String mobileNumber){
@@ -39,6 +64,15 @@ public class AccountsController {
 
     }
 
+    @Operation(
+            summary = "Update Account details REST API",
+            description = "REST API to update Customer & Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "417", description = "Expectation Failed"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO) {
         boolean isUpdated = iAccountsService.updateAccount(customerDTO);
@@ -49,11 +83,20 @@ public class AccountsController {
                     .body(new ResponseDTO(STATUS_200, MESSAGE_200));
         }else {
             return ResponseEntity
-                    .status(INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO(STATUS_500, MESSAGE_500));
+                    .status(EXPECTATION_FAILED)
+                    .body(new ResponseDTO(STATUS_417, MESSAGE_417_UPDATE));
         }
     }
 
+    @Operation(
+            summary = "Delete Account details REST API",
+            description = "REST API to delete Customer & Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "200", description = "Expectation Failed"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                                 String mobileNumber){
@@ -64,8 +107,8 @@ public class AccountsController {
                     .body(new ResponseDTO(STATUS_200, MESSAGE_200));
         }else {
             return ResponseEntity
-                    .status(INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO(STATUS_500, MESSAGE_500));
+                    .status(EXPECTATION_FAILED)
+                    .body(new ResponseDTO(STATUS_417, MESSAGE_417_DELETE));
         }
     }
 
